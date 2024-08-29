e37b14b25804895ab0a0b8c90239d49976d16752
Abdalrhman reda <abdalrhmanr444@gmail.com>
2024-08-29 12:44:55 +0300

    solve the problem

--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -14,24 +14,21 @@ import '../widgets/slider_products.dart';
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
-
-
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-  final List<Product> products = const [
-   // Product('جاكيت نسائي طويل سادة',
-     //'نشتي',
-     // 259.00,
-    // imageUrl: 'assets/images/photo_2024-08-16_22-50-16.jpg'
-     // ),
-   // Product('جاكيت طويل بأكمام طويلة', 'نشتي',
-   //  269.00, 'assets/image2.jpg'
-    // ),
-    // أضف المزيد من المنتجات هنا
-  ];
+final List<Product> products = const [
+  // Product('جاكيت نسائي طويل سادة',
+  //'نشتي',
+  // 259.00,
+  // imageUrl: 'assets/images/photo_2024-08-16_22-50-16.jpg'
+  // ),
+  // Product('جاكيت طويل بأكمام طويلة', 'نشتي',
+  //  269.00, 'assets/image2.jpg'
+  // ),
+  // أضف المزيد من المنتجات هنا
+];
 
 class _HomeScreenState extends State<HomeScreen> {
   List<String> categories = [
@@ -40,120 +37,144 @@ class _HomeScreenState extends State<HomeScreen> {
     'الرجال',
     'الأطفال',
     'الأحذية',
-    
-   // 'الإلكترونيات',
+
+    // 'الإلكترونيات',
     //'المنزل',
-   // 'الأثاث',
+    // 'الأثاث',
   ];
   int _current = 0;
-  
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-
-      child: Column(
-        children: [
-          SizedBox(
-            height: 50,
-            child: ListView.separated(
-              separatorBuilder: (context, index) => const SizedBox(width: 30),
-              scrollDirection: Axis.horizontal,
-              itemCount: categories.length,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _current = index;
-                    });
-                  },
-                  child: Column(
-                    children: [
-                      Text(
-                      
-                        categories[index],
-                        style: TextStyle(
-                          color: _current == index
-                              ? AppColors.kPrimaryColor
-                              : AppColors.kBlackColor,
-                          fontSize: 20,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      _current == index ? myDivider(70, 4) : Container(),
-                    ],    
-                  ),
-                );
-                
-              },
-            ),
-          ),
-          ListOfCircleProduct(),
-          SizedBox(height: 15),
-          SliderProducts(),
-          SizedBox(height: 20),
-          
-          Padding(
-            padding: const EdgeInsets.only(left:10),
-            child: Text("تسوق حسب المتجر  ",
-             style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              ),),
-          ),
-             SizedBox(height: 20,),
-      
-               SingleChildScrollView(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+              child: ListView.separated(
+                separatorBuilder: (context, index) => const SizedBox(width: 30),
                 scrollDirection: Axis.horizontal,
-                 child: Row(children: [
-                   categor(width: 170, imagem: "assets/images/photo_2024-08-21_23-30-45.jpg", heightm: 40),
-                    categor(width: 170, imagem: "assets/images/photo_2024-08-21_23-43-54.jpg", heightm: 40),
-                     categor(width: 170, imagem: "assets/images/photo_2024-08-21_23-47-35.jpg", heightm: 40),
-                 ],),
-               ) ,
-               SizedBox(height: 20,),
-               Text("                              cLow REClPE  أحدث تشكيلة من ",
-               style: TextStyle(fontSize: 20,
-               fontWeight: FontWeight.bold,
-      
-               ),
-               ),   
-        ],
+                itemCount: categories.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _current = index;
+                      });
+                    },
+                    child: Column(
+                      children: [
+                        Text(
+                          categories[index],
+                          style: TextStyle(
+                            color: _current == index
+                                ? AppColors.kPrimaryColor
+                                : AppColors.kBlackColor,
+                            fontSize: 20,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        _current == index ? myDivider(70, 4) : Container(),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            const ListOfCircleProduct(),
+            const SizedBox(height: 15),
+            const SliderProducts(),
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text(
+                "تسوق حسب المتجر",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  categor(
+                      width: 170,
+                      imagem: "assets/images/photo_2024-08-21_23-30-45.jpg",
+                      heightm: 40),
+                  categor(
+                      width: 170,
+                      imagem: "assets/images/photo_2024-08-21_23-43-54.jpg",
+                      heightm: 40),
+                  categor(
+                      width: 170,
+                      imagem: "assets/images/photo_2024-08-21_23-47-35.jpg",
+                      heightm: 40),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "  cLow REClPE  أحدث تشكيلة من ",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ProductListPage(),
+          ],
+        ),
       ),
     );
   }
 }
+
 ///////////////////
 class categor extends StatelessWidget {
-  const categor({super.key,
-   required this.width,
-    required this.imagem,
-     required this.heightm
-     });
-    final  double heightm;
-   final  double width;
-   final String imagem;
+  const categor(
+      {super.key,
+      required this.width,
+      required this.imagem,
+      required this.heightm});
+  final double heightm;
+  final double width;
+  final String imagem;
   // final String onTapp;
   @override
-  Widget  build(BuildContext context) {
-    return  Row(
-    children: [
-       GestureDetector(          
-             child: Image.asset(imagem,
-             height:heightm ,
-               width: width,),    
-              onTap: (){
-                Navigator.push(context, 
-                MaterialPageRoute(builder: (context){
-                  return ProfileScreen();
-                })
-                );
-              },
-            ),
-    ],
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        GestureDetector(
+          child: Image.asset(
+            imagem,
+            height: heightm,
+            width: width,
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return const ProfileScreen();
+                },
+              ),
+            );
+          },
+        ),
+      ],
     );
   }
 }
-
--- lib/features/home/ui/screens/product_screen.dart
+++ lib/features/home/ui/screens/product_screen.dart
@@ -1,98 +1,101 @@
 import 'package:flutter/material.dart';
 
-/*class ProductGridPage extends StatelessWidget {
-  const ProductGridPage({super.key});
-
-  final  List <String> Products = const [
-
-   ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold (
-     
-    );
-  }
-}*/
-
-
 class ProductListPage extends StatelessWidget {
   final List<Product> products = [
-   // Product('جاكيت نسائي طويل سادة',
-     //'نشتي',
-     // 259.00,
-    // imageUrl: 'assets/images/photo_2024-08-16_22-50-16.jpg'
-     // ),
-   // Product('جاكيت طويل بأكمام طويلة', 'نشتي',
-   //  269.00, 'assets/image2.jpg'
-    // ),
+    Product(
+      'جاكيت نسائي طويل سادة',
+      'نشتي',
+      'assets/images/photo_2024-08-16_22-50-16.jpg',
+      259.00,
+    ),
+    Product(
+      'جاكيت نسائي طويل سادة',
+      'نشتي',
+      'assets/images/photo_2024-08-16_22-50-16.jpg',
+      259.00,
+    ),
+    Product(
+      'جاكيت نسائي طويل سادة',
+      'نشتي',
+      'assets/images/photo_2024-08-16_22-50-16.jpg',
+      259.00,
+    ),
+    Product(
+      'جاكيت نسائي طويل سادة',
+      'نشتي',
+      'assets/images/photo_2024-08-16_22-50-16.jpg',
+      259.00,
+    ),
+
     // أضف المزيد من المنتجات هنا
   ];
 
+  ProductListPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-   
-    return  
-       GridView.builder(  
-        padding: EdgeInsets.all(10),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          childAspectRatio: 0.7,
-        ),
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          final product = products[index];
-          return GestureDetector(
-            onTap: () {
-             // Navigator.push(
-               // context,
-               // MaterialPageRoute(
-                 // builder: (context) => ProductDetailPage(product: product),
-                //),
-              //);
-            },
-            child: Card(
-              shape: RoundedRectangleBorder( 
-                borderRadius: BorderRadius.circular(15),
-              ),
-              elevation: 4,
-              child: Column(
-                children: [
-                  Image.asset(product.imageUrl, fit: BoxFit.cover),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(product.name, style: TextStyle(fontSize: 16)),
-                        SizedBox(height: 5),
-                        Text(product.brand, style: TextStyle(color: Colors.grey)),
-                        SizedBox(height: 10),
-                        Text(
-                          'SAR ${product.price.toStringAsFixed(2)}',
-                          style: TextStyle(color: Colors.green, fontSize: 16),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+    return GridView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      padding: const EdgeInsets.all(10),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        childAspectRatio: 0.7,
+      ),
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        final product = products[index];
+        return GestureDetector(
+          onTap: () {
+            // Navigator.push(
+            // context,
+            // MaterialPageRoute(
+            // builder: (context) => ProductDetailPage(product: product),
+            //),
+            //);
+          },
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
             ),
-          );
-        },
-             );
-    
+            elevation: 4,
+            child: Column(
+              children: [
+                Image.asset(product.imageUrl, fit: BoxFit.cover),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(product.name, style: const TextStyle(fontSize: 16)),
+                      const SizedBox(height: 5),
+                      Text(product.brand,
+                          style: const TextStyle(color: Colors.grey)),
+                      const SizedBox(height: 10),
+                      Text(
+                        'SAR ${product.price.toStringAsFixed(2)}',
+                        style:
+                            const TextStyle(color: Colors.green, fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
 
 class Product {
   final String name;
   final String brand;
-  final double price ;
+  final double price;
   final String imageUrl;
-   
 
-   Product (this.name,this.brand, this.imageUrl, this.price);
+  Product(this.name, this.brand, this.imageUrl, this.price);
 }
